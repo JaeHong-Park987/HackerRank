679c9d209a214de302a17a1f77f1dff21a6e3d31
JaeHong-Park987 <jar987@naver.com>
2023-08-22 17:41:32 +0900

    Weather Observation Station 5 풀이2
    해커랭크의 경우 결과를 도출하기 위해 하나의 쿼리문으로 짜지 않아도 되기 때문에 두개의 쿼리문으로 결과를 도출할 수 있다.

--- Weather Observation Station 5.sql
+++ Weather Observation Station 5.sql
@@ -1,23 +1,14 @@
 # 문제 : 가장 짧고 긴 CITY 이름과 각각의 길이(예: 이름의 문자 수)를 사용하여 STATION에서 두 도시를 쿼리합니다. 
 #       가장 작은 도시나 가장 큰 도시가 두 개 이상인 경우 알파벳 순서로 가장 먼저 나오는 도시를 선택하세요.
 SELECT
-  CITY,
-  L
-FROM (SELECT
-        CITY,
-        LENGTH(CITY) AS L
-      FROM STATION
-      ORDER BY L, CITY
-      LIMIT 1) AS C1
-
-UNION
-
+    CITY,
+    LENGTH(CITY) AS L
+FROM STATION
+ORDER BY L, CITY
+LIMIT 1;
 SELECT
-  CITY,
-  L
-FROM (SELECT
-        CITY,
-        LENGTH(CITY) AS L
-      FROM STATION
-      ORDER BY L DESC, CITY
-      LIMIT 1) AS C2
+    CITY,
+    LENGTH(CITY) AS L
+FROM STATION
+ORDER BY L DESC, CITY
+LIMIT 1;

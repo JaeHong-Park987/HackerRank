cfea85b8f4ab2254b63687b6b3e00176e993f6eb
JaeHong-Park987 <jar987@naver.com>
2023-08-28 17:04:06 +0900

    Weather Observation Station 11 풀이

---
+++ Weather Observation Station 11.sql
@@ -0,0 +1,6 @@
+# 문제 : STATION에서 모음으로 시작하지 않거나 모음으로 끝나지 않는 CITY 이름 목록을 쿼리합니다. 결과에는 중복된 내용이 포함될 수 없습니다.
+SELECT
+    DISTINCT(CITY)
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN ("A","E","I","O","U")
+OR RIGHT(CITY,1) NOT IN ("A","E","I","O","U")
